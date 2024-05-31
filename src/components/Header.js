5123682a38bd8062efd4474840e71db76c77b729
Amitkumar308 <amitkumar6377966427@gmail.com>
2024-06-01 03:44:44 +0530

    Merge branch 'main' of https://github.com/Amitkumar1308/MakeUp1 into main

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,21 @@ const Header = () => (
   <header className="header">
     <img src={logo} alt="Airblack Beauty Club" className="logo" />
     <h1>Professional Online Makeup Course</h1>
-    <p>Rated 4.8/5</p>
-    <p>Certification Programme</p>
-    <p>India's No.1 Online Makeup Course</p>
+    <div className="sub-header">
+      <span className="rated">Certification Programme</span>
+      <span className="rated">Rated 4.8/5</span>
+    </div>
+    <div className="benefits">
+      <span className="benefit">
+        <span className="checkmark">&#10004;</span> India's No.1 Online Makeup Course&nbsp;
+      </span>
+      {/* <span className="benefit">
+        <span className="checkmark">&#10004;</span> Learn by LIVE Do-it-Together Classes&nbsp;
+      </span>
+      <span className="benefit">
+        <span className="checkmark">&#10004;</span> Unlimited Practice Session to master skills&nbsp;
+      </span> */}
+    </div>
   </header>
 );
 
